50db507fe4d4169ca2758c9991664ffafc69ac2e
Nathan Vander Wilt <nate@calftrail.com>
2022-02-17 12:07:55 -0800

    add failing tests for #33

--- test/index.js
+++ test/index.js
@@ -12,6 +12,11 @@ describe('.sign(val, secret)', function(){
     var val = cookie.sign('hello', 'luna');
     val.should.not.equal('hello.DGDUkGlIkCzPz+C0B064FNgHdEjox7ch8tOBGslZ5QI');
   })
+  it('should accept non-string secrets', function(){
+    var key = Buffer.from("A0ABBC0C", 'hex'),
+        val = cookie.sign('hello', key);
+    val.should.equal('hello.hIvljrKw5oOZtHHSq5u+MlL27cgnPKX77y7F+x5r1to');
+  })
 })
 
 describe('.unsign(val, secret)', function(){
@@ -29,5 +34,10 @@ describe('.unsign(val, secret)', function(){
     cookie.unsign('garbage.'+val, pwd).should.be.false();
     cookie.unsign(val+'.garbage', pwd).should.be.false();
     cookie.unsign(val+'garbage', pwd).should.be.false();
+  })
+  it('should accept non-string secrets', function(){
+    var key = Uint8Array.from([0xA0, 0xAB, 0xBC, 0x0C]),
+        val = cookie.unsign('hello.hIvljrKw5oOZtHHSq5u+MlL27cgnPKX77y7F+x5r1to', key);
+    val.should.equal('hello');
   })
 })
